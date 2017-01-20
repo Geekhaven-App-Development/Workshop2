85a50732737fe56cddd1652e993af9a3dd7bdec6
Pranjal Paliwal <paliwal.pranjal83@gmail.com>
2017-01-21 02:39:42 +0530

    Added Files

---
+++ app/src/main/java/com/betterclever/myapplication/MainActivity.java
@@ -0,0 +1,65 @@
+package com.betterclever.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText usernameET;
+    EditText passwordET;
+    Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        usernameET = (EditText) findViewById(R.id.txt);
+        passwordET = (EditText) findViewById(R.id.password);
+        loginButton = (Button) findViewById(R.id.loginbutton);
+
+        final String actualUsername = "A_Username";
+        final String actualPassword = "Password";
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String usernameEntered = usernameET.getText().toString();
+                String passswordEntred = passwordET.getText().toString();
+
+                if(actualPassword.equals(passswordEntred) &&
+                        usernameEntered.equals(actualUsername)){
+                    Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(MainActivity.this,SecondActivity.class));
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
